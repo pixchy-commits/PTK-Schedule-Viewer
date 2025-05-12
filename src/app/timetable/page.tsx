39e1edf71e93bebb038dbdaec5464ef3ee503092
Pichayut Pongpeaw <6341430@schoolptk.ac.th>
2025-05-12 22:15:56 +0700

    fix: Comment out unused Image import in TimetablePage component

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 import { useTimetableWithRefetch } from '@/hooks/useTimetable';
 import { timetableService } from '@/services/timetableService';
 import { fetchAndProcessTimetableData, exportTimetableToCSV } from '@/utils/timetableUtils';
